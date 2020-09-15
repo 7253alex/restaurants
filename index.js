14f2f37ff247527366dd6d4064753dcb39f0d587
Alexander Lo <7253alex@gmail.com>
2020-09-14 17:56:14 -0700

    sendFile

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.get("/search", async(req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
