f66e3840bd3924f70dc358116d27fcbe8675bb9a
Alexander Lo <7253alex@gmail.com>
2020-09-14 19:02:45 -0700

    fix

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (request, response) => {
+  //   response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 // routes
