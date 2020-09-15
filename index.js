c4741c66a7a63d043a9edeb0024fcd9780dec52c
Alexander Lo <7253alex@gmail.com>
2020-09-14 18:50:47 -0700

    fix

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  //server static content
-  //npm run build
-  app.use(express.static(path.join(__dirname, "client/build")));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
   app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
