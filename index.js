c56cb57c56f4bfdbef0a391f3d64b0f7b0e24a52
7253alex <hoalex08@gmail.com>
2020-09-14 22:17:04 -0700

    asdas

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (request, response) => {
+  //   response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 // routes
